e4e16829f53d52cb463753a60780f712bccca8cb
Theo Martin <theo.martin22@icloud.com>
2016-09-13 17:15:04 +0200

    petite modif

--- contact.php
+++ contact.php
@@ -5,19 +5,19 @@
                   <div class="style-form">
                         <form>
                           <div class="form-group">
-                            <label for="exampleInputEmail1">Nom</label>
+                            <label for="exampleInputEmail1">Nom          <span id="asterix-red">*</span></label>
                             <input type="text" class="form-control" id="exampleInputEmail1">
                         </div>
                         <div class="form-group">
-                            <label for="exampleInputPassword1">Téléphone</label>
+                            <label for="exampleInputPassword1">Téléphone <span id="asterix-red">*</span></label>
                             <input type="tel" class="form-control" id="exampleInputPassword1">
                         </div>
                         <div class="form-group">
-                            <label for="email">E-Mail</label>
+                            <label for="email">E-Mail                    <span id="asterix-red">*</span></label>
                             <input type="email" class="form-control" id="email">
                         </div>
                         <div class="form-group">
-                            <label for="text">Votre message</label>
+                            <label for="text">Votre message              <span id="asterix-red">*</span></label>
                             <textarea class="form-control" rows="3"></textarea>
                         </div>
 
--- inscription.php
+++ inscription.php
@@ -23,15 +23,6 @@
 	</div>
 	<button type="submit"      class="btn btn-default">Envoyez</button>
 
-<!-- 	<textarea name="editor1" id="editor1" rows="10" cols="80">
-                This is my textarea to be replaced with CKEditor.
-            </textarea>
-            <script>
-                // Replace the <textarea id="editor1"> with a CKEditor
-                // instance, using default configuration.
-                CKEDITOR.replace( 'editor1' );
-            </script> -->
-
 </form>
 
 <?php include("footer.php"); ?>
