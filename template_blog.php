e9bff9b599d8489e543a9ba55daef8f6626b3a8e
Theo Martin <theo.martin22@icloud.com>
2016-09-13 16:55:36 +0200

    Découpage et import du css/scss

--- template_blog.php
+++ template_blog.php
@@ -6,53 +6,55 @@
 	</div>
 	<div class="col-md-9">
 		<div class="row">
-			<div class="col-md-12">
-				<div class="row">
-					<div class="col-md-3">
-						<img class="img-responsive" src="img/front-end-vs-back-end.jpg" alt="">
+			<div class="style-display-article">
+				<div class="col-md-12">
+					<div class="row">
+						<div class="col-md-3">
+							<img class="img-responsive" src="img/front-end-vs-back-end.jpg" alt="">
+						</div>
+						<div class="col-md-9">
+							<h3><a href="template_article.php">Titre article</a></h3>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam sequi magni sint et nemo reiciendis nam adipisci dolor, commodi nesciunt dolorem ipsum expedita, modi perspiciatis numquam repellat reprehenderit. Esse, ratione?</p>
+						</div>
 					</div>
-					<div class="col-md-9">
-						<h3><a href="template_article.php">Titre article</a></h3>
-						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam sequi magni sint et nemo reiciendis nam adipisci dolor, commodi nesciunt dolorem ipsum expedita, modi perspiciatis numquam repellat reprehenderit. Esse, ratione?</p>
-					</div>
+					<div class="separate-line"></div>
 				</div>
-				<div class="separate-line"></div>
-			</div>
-			<div class="col-md-12">
-				<div class="row">
-					<div class="col-md-3">
-						<img class="img-responsive" src="img/front-end-vs-back-end.jpg" alt="">
+				<div class="col-md-12">
+					<div class="row">
+						<div class="col-md-3">
+							<img class="img-responsive" src="img/front-end-vs-back-end.jpg" alt="">
+						</div>
+						<div class="col-md-9">
+							<h3><a href="template_article.php">Titre article</a></h3>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam sequi magni sint et nemo reiciendis nam adipisci dolor, commodi nesciunt dolorem ipsum expedita, modi perspiciatis numquam repellat reprehenderit. Esse, ratione?</p>
+						</div>
 					</div>
-					<div class="col-md-9">
-						<h3><a href="template_article.php">Titre article</a></h3>
-						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam sequi magni sint et nemo reiciendis nam adipisci dolor, commodi nesciunt dolorem ipsum expedita, modi perspiciatis numquam repellat reprehenderit. Esse, ratione?</p>
-					</div>
+					<div class="separate-line"></div>
 				</div>
-				<div class="separate-line"></div>
-			</div>
-			<div class="col-md-12">
-				<div class="row">
-					<div class="col-md-3">
-						<img class="img-responsive" src="img/front-end-vs-back-end.jpg" alt="">
+				<div class="col-md-12">
+					<div class="row">
+						<div class="col-md-3">
+							<img class="img-responsive" src="img/front-end-vs-back-end.jpg" alt="">
+						</div>
+						<div class="col-md-9">
+							<h3><a href="template_article.php">Titre article</a></h3>
+							<p>lLorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam sequi magni sint et nemo reiciendis nam adipisci dolor, commodi nesciunt dolorem ipsum expedita, modi perspiciatis numquam repellat reprehenderit. Esse, ratione?</p>
+						</div>
 					</div>
-					<div class="col-md-9">
-						<h3><a href="template_article.php">Titre article</a></h3>
-						<p>lLorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam sequi magni sint et nemo reiciendis nam adipisci dolor, commodi nesciunt dolorem ipsum expedita, modi perspiciatis numquam repellat reprehenderit. Esse, ratione?</p>
-					</div>
+					<div class="separate-line"></div>
 				</div>
-				<div class="separate-line"></div>
-			</div>
-			<div class="col-md-12">
-				<div class="row">
-					<div class="col-md-3">
-						<img class="img-responsive" src="img/front-end-vs-back-end.jpg" alt="">
+				<div class="col-md-12">
+					<div class="row">
+						<div class="col-md-3">
+							<img class="img-responsive" src="img/front-end-vs-back-end.jpg" alt="">
+						</div>
+						<div class="col-md-9">
+							<h3><a href="template_article.php">Titre article</a></h3>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam sequi magni sint et nemo reiciendis nam adipisci dolor, commodi nesciunt dolorem ipsum expedita, modi perspiciatis numquam repellat reprehenderit. Esse, ratione?</p>
+						</div>
 					</div>
-					<div class="col-md-9">
-						<h3><a href="template_article.php">Titre article</a></h3>
-						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam sequi magni sint et nemo reiciendis nam adipisci dolor, commodi nesciunt dolorem ipsum expedita, modi perspiciatis numquam repellat reprehenderit. Esse, ratione?</p>
-					</div>
+					<div class="separate-line"></div>
 				</div>
-				<div class="separate-line"></div>
 			</div>
 		</div>
 	</div>
