1a423a9daed3797c2f05bd92961b66828e11e9ae
Theo Martin <theo.martin22@icloud.com>
2016-09-15 15:21:40 +0200

    last mod

--- menu_blog.php
+++ menu_blog.php
@@ -1,7 +1,25 @@
 <div class="menu-blog">
 
-	<h4>Catégories</h4>
+<div class="row">
+	<div class="col-md-4">
+		<img src="img/profil1.png" alt="Photos de profil" class="img-responsive">
+	</div>
+	<div class="col-md-7">
+		<span>Théo</span>
+		<br>
+		<span>Niveaux 3</span>
+		<br>
+		<div class="progress">
+		  <div class="progress-bar progress-bar-info progress-bar-striped active" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width: 80%">
+		    <!-- <span class="sr-only">20% Complete</span> -->
+		  </div>
+		</div>
+	</div>
+</div>
+
+	<!-- <h4>Catégories</h4> -->
 	<ul>
+	<li id="border-top-menu"></li>
 		<li><a class="color5" href="#">Qu'est ce que.. ?!</a></li>
 		<li><a class="color1" href="#">HTML/CSS</a></li>
 		<li><a class="color2" href="#">PHP</a></li>
@@ -10,7 +28,7 @@
 	</ul>
 
 
-	<h4>Les derniers articles</h4>
+<!-- 	<h4>Les derniers articles</h4> -->
 	<ul>
 		<li><a class="color1" href="#">Example</a></li>
 		<li><a class="color2" href="#">Example</a></li>
